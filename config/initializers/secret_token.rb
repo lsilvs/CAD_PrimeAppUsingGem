4fe4cd9ec6e7ed960cacf883111557f742bbcc3c
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-02-27 13:50:57 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gemprimeapp::Application.config.secret_key_base = 'd447210ba0de170cb48a4a821af4266ad877b9572644fe92827aa67574a3da84a600c2c5d6dc752a09583fe80526bd3be0ea81b8bab9271354ce7e206d2b01c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gemprimeapp::Application.config.session_store :cookie_store, key: '_gemprimeapp_session'
